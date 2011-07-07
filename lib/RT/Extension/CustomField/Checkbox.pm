fa74627e82007c9c562f49594e194106a9543dea
Руслан У. Закиров <Ruslan.Zakirov@gmail.com>
2011-07-07 23:07:48 +0000

    Import of RUZ/RT-Extension-CustomField-Checkbox-0.03 from CPAN.
    gitpan-cpan-distribution: RT-Extension-CustomField-Checkbox
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         RUZ/RT-Extension-CustomField-Checkbox-0.03.tar.gz
    gitpan-cpan-author:       RUZ
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/CustomField/Checkbox.pm
+++ lib/RT/Extension/CustomField/Checkbox.pm
@@ -4,7 +4,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 NAME
 
@@ -12,11 +12,33 @@ RT::Extension::CustomField::Checkbox - extension for RT to add checkboxes and ra
 
 =head1 DESCRIPTION
 
-Install it, register within plugin in the config. Enjoy.
+Install it, register within @Plugins in the config. Enjoy.
+
+=head1 ATTENTION FOR USERS OF RT 4.0
+
+You don't need this extension. RT 4.0 and newer has this
+functionality build in.
+
+If you're B<upgrading> from RT 3.8 and used this extension
+then additinal steps may be required. At least RT 4.0.0
+and 4.0.1 has no upgrade script to convert custom fields
+created by this extensions into 4.x CFs.
+
+Run the following SQL:
+
+    UPDATE CustomFields SET Type = 'Select' AND RenderType = 'List'
+    WHERE Type = 'SelectCheckbox';
+
+Delete this extension from @Plugins in the config file.
+
+Hopefuly in RT 4.0.2 all this would be automated.
 
 =cut
 
 # code goes here
+
+die "\n\nYou don't need this extension. RT 4.0 and newer has this functionality build in. Read documentation for upgrade instructions.\n\n"
+    if $RT::VERSION =~ /^[4-9]\./;
 
 use RT::CustomField;
 $RT::CustomField::FieldTypes{'SelectCheckbox'} = [
